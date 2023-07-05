aff66fc55f8b48f3d0adc0640a27679a2be28c0a
Lianming Du <adullb@qq.com>
2023-07-05 14:29:13 +0800

    Update macos_install_openmm.sh

--- macos_install_openmm.sh
+++ macos_install_openmm.sh
@@ -1,5 +1,5 @@
 pip install numpy cython
-brew install doxygen swig wget cmake
+#brew install doxygen swig wget cmake
 wget https://github.com/openmm/openmm/archive/refs/tags/8.0.0.tar.gz
 tar xzvf 8.0.0.tar.gz
 cd openmm-8.0.0
@@ -8,4 +8,4 @@ cd build
 cmake --version
 cmake ..
 make
-make install
+sudo make install
